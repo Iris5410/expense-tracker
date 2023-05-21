4085ba84bd371de2d8d5c55ef6ee9255b0dd6042
yiyun <ccjhs70325@gmail.com>
2023-05-21 19:04:20 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const routes = require('./routes')
 const app = express()
 const port = 3000
@@ -14,6 +15,13 @@ require('./config/mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(express.urlencoded({ extended: true }))
 
