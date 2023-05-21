23af81344804113fb399faf8dae7fa6fd9911818
yiyun <ccjhs70325@gmail.com>
2023-05-21 19:49:29 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -16,11 +16,28 @@ router.get('/register', (req, res) => {
 // 註冊使用者
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
 
   User.findOne({ email }) .then((user) => {
     if (user) {
-      console.log('User already exists.')
+      errors.push({ message: '這個 Email 已經註冊過了。' })
       res.render('register', {
+        errors,
         name,
         email,
         password,
@@ -48,6 +65,7 @@ router.post('/login', passport.authenticate('local', {
 // 使用者登出
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '成功登出。')
   res.redirect('/users/login')
 })
 
