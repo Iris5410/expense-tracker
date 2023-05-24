af390dbb1559da026d60ad173381132ebd08e37c
yiyun <ccjhs70325@gmail.com>
2023-05-24 12:52:36 +0800

    feat: add view records by category

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,6 +5,7 @@ const Category = require('../../models/category')
 const Record = require('../../models/record')
 const { totalAmount } = require('../../utilities/amount')
 
+// 所有紀錄
 router.get('/', async (req, res) => {
   try {
     const userId = req.user._id
@@ -25,5 +26,25 @@ router.get('/', async (req, res) => {
     console.log(err)
   }
 })
+
+// 選取特定類別的紀錄
+router.get('/search', async (req, res) => {
+  const category = req.query.category
+  const userId = req.user._id
+  try {
+    const categoryWithId = await Category.findOne({ name: category }) .lean()
+    const records = await Record.find({ userId, categoryId: categoryWithId._id}) .lean()
+    const sortedRecords = await Promise.all(records.map(async record => {
+    return {
+      ...record,
+      date: record.date.toLocaleDateString(),
+      image: categoryWithId.icon
+    }}))
+    res.render('index', { records: sortedRecords, totalAmount: totalAmount(records) })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 
 module.exports = router
